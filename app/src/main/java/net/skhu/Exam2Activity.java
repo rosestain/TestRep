a493505fe01dc969eb9862a451551eb03182c6c4
rosestain <yunie2004@gmail.com>
2020-05-04 17:43:36 +0900

    Mid

--- app/src/main/java/net/skhu/Exam2Activity.java
+++ app/src/main/java/net/skhu/Exam2Activity.java
@@ -2,7 +2,11 @@ package net.skhu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AlertDialog;
 import android.os.Bundle;
+import android.content.DialogInterface;
+import android.view.View;
+import android.widget.Toast;
 
 public class Exam2Activity extends AppCompatActivity {
 
